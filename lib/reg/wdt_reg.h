c8736de194f1b37c29a553c558c2869b12d7e521
Константин Скорость <dltech174@gmail.com>
2022-06-03 00:49:33 +0700

    WDT regs

--- lib/reg/wdt_reg.h
+++ lib/reg/wdt_reg.h
@@ -20,19 +20,65 @@
  */
 #include "memorymap.h"
 
-/* */
-#define STARTWDT   MMIO32(WDT + 0x000)
-
 /*************Tasks************************/
-/*  */
-#define START   MMIO32(WDT + 0x)
+/* Start the watchdog */
+#define STARTWDT        MMIO32(WDT + 0x000)
 
 /************Events************************/
-/*  */
-#define READY   MMIO32(WDT + 0x)
+/* Watchdog timeout */
+#define TIMEOUTWDT      MMIO32(WDT + 0x100)
 
 /************Registers*********************/
-/*  */
-#define SHORTS        MMIO32(WDT + 0x)
+/* Enable interrupt */
+#define INTENSETWDT     MMIO32(WDT + 0x304)
+// Write '1' to Enable interrupt on TIMEOUT event.
+#define TIMEOUTW    0x1
+
+/* Disable interrupt */
+#define INTENCLRWDT     MMIO32(WDT + 0x308)
+// Write '1' to Clear interrupt on TIMEOUT event.
+
+/* Run status */
+#define RUNSTATUSWDT    MMIO32(WDT + 0x400)
+// Indicates whether or not the watchdog is running
+#define RUNSTATUSW  0x1
+
+/* Request status */
+#define REQSTATUSWDT    MMIO32(WDT + 0x404)
+// Request status for RR[n] register
+#define RR0W    0x01
+#define RR1W    0x02
+#define RR2W    0x04
+#define RR3W    0x08
+#define RR4W    0x10
+#define RR5W    0x20
+#define RR6W    0x40
+#define RR7W    0x80
+
+/* Counter reload value */
+#define CRVWDT          MMIO32(WDT + 0x504)
+// Counter reload value in number of cycles of the 32.768 kHz. CRV[31:0]
+
+/* Enable register for reload request registers */
+#define RRENWDT         MMIO32(WDT + 0x508)
+// Enable or disable RR[n] register
+
+/* Configuration register */
+#define CONFIGWDT       MMIO32(WDT + 0x50c)
+//Configure the wdt to either be paused, or kept running, while the CPU is sleep
+#define SLEEPW  0x8
+//Conf the wdt to either be paused, or kept running, while the CPU is halted
+#define HALTW   0x1
+
+/* Reload request n */
+#define RR0WDT          MMIO32(WDT + 0x600)
+#define RR1WDT          MMIO32(WDT + 0x604)
+#define RR2WDT          MMIO32(WDT + 0x608)
+#define RR3WDT          MMIO32(WDT + 0x60c)
+#define RR4WDT          MMIO32(WDT + 0x610)
+#define RR5WDT          MMIO32(WDT + 0x614)
+#define RR6WDT          MMIO32(WDT + 0x618)
+#define RR7WDT          MMIO32(WDT + 0x61c)
+// Reload request register. Value to request a reload of the watchdog timer.
 
 #endif
