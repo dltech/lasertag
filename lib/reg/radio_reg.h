fb5311e6f97d47bcbd03caee9a007db127098a77
Константин Скорость <dltech174@gmail.com>
2022-06-03 03:23:00 +0700

    timer regs

--- lib/reg/radio_reg.h
+++ lib/reg/radio_reg.h
@@ -20,20 +20,162 @@
  */
 #include "memorymap.h"
 
-/* */
-#define TXEN   MMIO32(RADIO + 0x000)
-
 /*************Tasks************************/
-/*  */
-#define START   MMIO32(RADIO + 0x)
+/* Enable RADIO in TX mode */
+#define TXENRADIO           MMIO32(RADIO + 0x000)
+/* Enable RADIO in RX mode */
+#define RXENRADIO           MMIO32(RADIO + 0x004)
+/* Start RADIO */
+#define STARTRADIO          MMIO32(RADIO + 0x008)
+/* Stop RADIO */
+#define STOPRADIO           MMIO32(RADIO + 0x00c)
+/* Disable RADIO */
+#define DISABLERADIO        MMIO32(RADIO + 0x010)
+/* Start the RSSI and take one single sample of the receive signal strength */
+#define RSSISTARTRADIO      MMIO32(RADIO + 0x014)
+/* Stop the RSSI measurement */
+#define RSSISTOPRADIO       MMIO32(RADIO + 0x018)
+/* Start the bit counter */
+#define BCSTARTRADIO        MMIO32(RADIO + 0x01c)
+/* Stop the bit counter */
+#define BCSTOPRADIO         MMIO32(RADIO + 0x020)
 
 /************Events************************/
-/*  */
-#define READY   MMIO32(RADIO + 0x)
+/* RADIO has ramped up and is ready to be started */
+#define READYRADIO          MMIO32(RADIO + 0x100)
+/* Address sent or received */
+#define ADDRESSRADIO        MMIO32(RADIO + 0x104)
+/* Packet payload sent or received */
+#define PAYLOADRADIO        MMIO32(RADIO + 0x108)
+/* Packet sent or received */
+#define ENDRADIO            MMIO32(RADIO + 0x10c)
+/* RADIO has been disabled */
+#define DISABLEDRADIO       MMIO32(RADIO + 0x110)
+/* A device address match occurred on the last received packet */
+#define DEVMATCHRADIO       MMIO32(RADIO + 0x114)
+/* No device address match occurred on the last received packet */
+#define DEVMISSRADIO        MMIO32(RADIO + 0x118)
+/*Sampling of receive signal strength complete. A new sample is ready for read*/
+#define RSSIENDRADIO        MMIO32(RADIO + 0x11c)
+/* Bit counter reached bit count value specified in the BCC register */
+#define BCMATCHRADIO        MMIO32(RADIO + 0x128)
 
 /************Registers*********************/
-/*  */
-#define SHORTS        MMIO32(RADIO + 0x)
+/* Shortcut register */
+#define SHORTSRADIO         MMIO32(RADIO + 0x200)
 
+/* Enable interrupt */
+#define INTENSETRADIO       MMIO32(RADIO + 0x304)
+
+/* Disable interrupt */
+#define INTENCLRRADIO       MMIO32(RADIO + 0x308)
+
+/* CRC status */
+#define CRCSTATUSRADIO      MMIO32(RADIO + 0x400)
+
+/* Received address */
+#define RXMATCHRADIO        MMIO32(RADIO + 0x408)
+
+/* CRC field of previously received packet */
+#define RXCRCRADIO          MMIO32(RADIO + 0x40c)
+
+/* Device address match index */
+#define DAIRADIO            MMIO32(RADIO + 0x410)
+
+/* Packet pointer */
+#define PACKETPTRRADIO      MMIO32(RADIO + 0x504)
+
+/* Frequency */
+#define FREQUENCYRADIO      MMIO32(RADIO + 0x508)
+
+/* Output power */
+#define TXPOWERRADIO        MMIO32(RADIO + 0x50c)
+
+/* Data rate and modulation */
+#define MODERADIO           MMIO32(RADIO + 0x510)
+
+/* Packet configuration register 0 */
+#define PCNF0RADIO          MMIO32(RADIO + 0x514)
+
+/* Packet configuration register 1 */
+#define PCNF1RADIO          MMIO32(RADIO + 0x518)
+
+/* Base address 0 */
+#define BASE0RADIO          MMIO32(RADIO + 0x51c)
+
+/* Base address 1 */
+#define BASE1RADIO          MMIO32(RADIO + 0x520)
+
+/* Prefixes bytes for logical addresses 0-3 */
+#define PREFIX0RADIO        MMIO32(RADIO + 0x524)
+
+/* Prefixes bytes for logical addresses 4-7 */
+#define PREFIX1RADIO        MMIO32(RADIO + 0x528)
+
+/* Transmit address select */
+#define TXADDRESSRADIO      MMIO32(RADIO + 0x52c)
+
+/* Receive address select */
+#define RXADDRESSESRADIO    MMIO32(RADIO + 0x530)
+
+/* CRC configuration */
+#define CRCCNFRADIO         MMIO32(RADIO + 0x534)
+
+/* CRC polynomial */
+#define CRCPOLYRADIO        MMIO32(RADIO + 0x538)
+
+/* CRC initial value */
+#define CRCINITRADIO        MMIO32(RADIO + 0x53c)
+
+/* Test features enable register. */
+#define TESTRADIO           MMIO32(RADIO + 0x540)
+
+/* Inter Frame Spacing in us */
+#define TIFSRADIO           MMIO32(RADIO + 0x544)
+
+/* RSSI sample */
+#define RSSISAMPLERADIO     MMIO32(RADIO + 0x548)
+
+/* Current radio state */
+#define STATERADIO          MMIO32(RADIO + 0x550)
+
+/* Data whitening initial value */
+#define DATAWHITEIVRADIO    MMIO32(RADIO + 0x554)
+
+/* Bit counter compare */
+#define BCCRADIO            MMIO32(RADIO + 0x560)
+
+/* Device address base segment n */
+#define DAB0RADIO           MMIO32(RADIO + 0x600)
+#define DAB1RADIO           MMIO32(RADIO + 0x604)
+#define DAB2RADIO           MMIO32(RADIO + 0x608)
+#define DAB3RADIO           MMIO32(RADIO + 0x60c)
+#define DAB4RADIO           MMIO32(RADIO + 0x610)
+#define DAB5RADIO           MMIO32(RADIO + 0x614)
+#define DAB6RADIO           MMIO32(RADIO + 0x618)
+#define DAB7RADIO           MMIO32(RADIO + 0x61c)
+
+/* Device address prefix n */
+#define DAP0RADIO           MMIO32(RADIO + 0x620)
+#define DAP1RADIO           MMIO32(RADIO + 0x624)
+#define DAP2RADIO           MMIO32(RADIO + 0x628)
+#define DAP3RADIO           MMIO32(RADIO + 0x62c)
+#define DAP4RADIO           MMIO32(RADIO + 0x630)
+#define DAP5RADIO           MMIO32(RADIO + 0x634)
+#define DAP6RADIO           MMIO32(RADIO + 0x638)
+#define DAP7RADIO           MMIO32(RADIO + 0x63c)
+
+/* Device address match configuration */
+#define DACNFRADIO          MMIO32(RADIO + 0x640)
+
+/* Trim value override register n */
+#define OVERRIDE0RADIO      MMIO32(RADIO + 0x724)
+#define OVERRIDE1RADIO      MMIO32(RADIO + 0x728)
+#define OVERRIDE2RADIO      MMIO32(RADIO + 0x72c)
+#define OVERRIDE3RADIO      MMIO32(RADIO + 0x730)
+#define OVERRIDE4RADIO      MMIO32(RADIO + 0x734)
+
+/* Peripheral power control */
+#define POWERRADIO          MMIO32(RADIO + 0xffc)
 
 #endif
--- lib/reg/timer_reg.h
+++ lib/reg/timer_reg.h
@@ -20,26 +20,135 @@
  */
 #include "memorymap.h"
 
-/* */
-
-
 /*************Tasks************************/
-/*  */
-#define STARTTIMER0   MMIO32(TIMER0 + 0x000)
-#define STARTTIMER1   MMIO32(TIMER1 + 0x000)
-#define STARTTIMER2   MMIO32(TIMER2 + 0x000)
-
+/* Start Timer */
+#define STARTTIMER0     MMIO32(TIMER0 + 0x000)
+#define STARTTIMER1     MMIO32(TIMER1 + 0x000)
+#define STARTTIMER2     MMIO32(TIMER2 + 0x000)
+/* Stop Timer */
+#define STOPTIMER0      MMIO32(TIMER0 + 0x004)
+#define STOPTIMER1      MMIO32(TIMER1 + 0x004)
+#define STOPTIMER2      MMIO32(TIMER2 + 0x004)
+/* Increment Timer (Counter mode only) */
+#define COUNTTIMER0     MMIO32(TIMER0 + 0x008)
+#define COUNTTIMER1     MMIO32(TIMER1 + 0x008)
+#define COUNTTIMER2     MMIO32(TIMER2 + 0x008)
+/* Clear time */
+#define CLEARTIMER0     MMIO32(TIMER0 + 0x00c)
+#define CLEARTIMER1     MMIO32(TIMER1 + 0x00c)
+#define CLEARTIMER2     MMIO32(TIMER2 + 0x00c)
+/* Shut down timer */
+#define SHUTDOWNTIMER0  MMIO32(TIMER0 + 0x010)
+#define SHUTDOWNTIMER1  MMIO32(TIMER1 + 0x010)
+#define SHUTDOWNTIMER2  MMIO32(TIMER2 + 0x010)
+/* Capture Timer value to CCn register */
+#define CAPTURE0TIMER0  MMIO32(TIMER0 + 0x040)
+#define CAPTURE0TIMER1  MMIO32(TIMER1 + 0x040)
+#define CAPTURE0TIMER2  MMIO32(TIMER2 + 0x040)
+#define CAPTURE1TIMER0  MMIO32(TIMER0 + 0x044)
+#define CAPTURE1TIMER1  MMIO32(TIMER1 + 0x044)
+#define CAPTURE1TIMER2  MMIO32(TIMER2 + 0x044)
+#define CAPTURE2TIMER0  MMIO32(TIMER0 + 0x048)
+#define CAPTURE2TIMER1  MMIO32(TIMER1 + 0x048)
+#define CAPTURE2TIMER2  MMIO32(TIMER2 + 0x048)
+#define CAPTURE3TIMER0  MMIO32(TIMER0 + 0x04c)
+#define CAPTURE3TIMER1  MMIO32(TIMER1 + 0x04c)
+#define CAPTURE3TIMER2  MMIO32(TIMER2 + 0x04c)
 /************Events************************/
-/*  */
-#define READY   MMIO32(TIMER0 + 0x)
-#define READY   MMIO32(TIMER1 + 0x)
-#define READY   MMIO32(TIMER2 + 0x)
+/* Compare event on CCn match */
+#define COMPARE0TIMER0  MMIO32(TIMER0 + 0x140)
+#define COMPARE0TIMER1  MMIO32(TIMER1 + 0x140)
+#define COMPARE0TIMER2  MMIO32(TIMER2 + 0x140)
+#define COMPARE1TIMER0  MMIO32(TIMER0 + 0x144)
+#define COMPARE1TIMER1  MMIO32(TIMER1 + 0x144)
+#define COMPARE1TIMER2  MMIO32(TIMER2 + 0x144)
+#define COMPARE2TIMER0  MMIO32(TIMER0 + 0x148)
+#define COMPARE2TIMER1  MMIO32(TIMER1 + 0x148)
+#define COMPARE2TIMER2  MMIO32(TIMER2 + 0x148)
+#define COMPARE3TIMER0  MMIO32(TIMER0 + 0x14c)
+#define COMPARE3TIMER1  MMIO32(TIMER1 + 0x14c)
+#define COMPARE3TIMER2  MMIO32(TIMER2 + 0x14c)
 
 /************Registers*********************/
-/*  */
-#define SHORTS        MMIO32(TIMER0 + 0x)
-#define SHORTS        MMIO32(TIMER1 + 0x)
-#define SHORTS        MMIO32(TIMER2 + 0x)
+/* Shortcut register */
+#define SHORTSTIMER0    MMIO32(TIMER0 + 0x200)
+#define SHORTSTIMER1    MMIO32(TIMER1 + 0x200)
+#define SHORTSTIMER2    MMIO32(TIMER2 + 0x200)
+// Shortcut between COMPARE[0] event and CLEAR task
+#define COMPARE0_CLEAR  0x001
+// Shortcut between COMPARE[1] event and CLEAR task
+#define COMPARE1_CLEAR  0x002
+// Shortcut between COMPARE[2] event and CLEAR task
+#define COMPARE2_CLEAR  0x004
+// Shortcut between COMPARE[3] event and CLEAR task
+#define COMPARE3_CLEAR  0x008
+// Shortcut between COMPARE[0] event and STOP task
+#define COMPARE0_STOP   0x100
+// Shortcut between COMPARE[1] event and STOP task
+#define COMPARE1_STOP   0x200
+// Shortcut between COMPARE[2] event and STOP task
+#define COMPARE2_STOP   0x400
+// Shortcut between COMPARE[3] event and STOP task
+#define COMPARE3_STOP   0x800
 
+/* Enable interrupt */
+#define INTENSETTIMER0  MMIO32(TIMER0 + 0x304)
+#define INTENSETTIMER1  MMIO32(TIMER1 + 0x304)
+#define INTENSETTIMER2  MMIO32(TIMER2 + 0x304)
+// Write '1' to Enable interrupt on COMPARE[0] event.
+#define COMPARE0T   0x10000
+// Write '1' to Enable interrupt on COMPARE[1] event.
+#define COMPARE1T   0x20000
+// Write '1' to Enable interrupt on COMPARE[2] event.
+#define COMPARE2T   0x40000
+// Write '1' to Enable interrupt on COMPARE[3] event.
+#define COMPARE3T   0x80000
+
+/* Disable interrupt */
+#define INTENCLRTIMER0  MMIO32(TIMER0 + 0x308)
+#define INTENCLRTIMER1  MMIO32(TIMER1 + 0x308)
+#define INTENCLRTIMER2  MMIO32(TIMER2 + 0x308)
+// Write '1' to Clear interrupt on COMPARE[0] event.
+// Write '1' to Clear interrupt on COMPARE[1] event.
+// Write '1' to Clear interrupt on COMPARE[2] event.
+// Write '1' to Clear interrupt on COMPARE[3] event.
+
+/* Timer mode selection */
+#define MODETIMER0      MMIO32(TIMER0 + 0x504)
+#define MODETIMER1      MMIO32(TIMER1 + 0x504)
+#define MODETIMER2      MMIO32(TIMER2 + 0x504)
+// Timer mode. 0 = Timer mode, 1 = Counter mode
+#define MODE_COUNTER    0x1
+
+/* Configure the number of bits used by the TIMER */
+#define BITMODETIMER0   MMIO32(TIMER0 + 0x508)
+#define BITMODETIMER1   MMIO32(TIMER1 + 0x508)
+#define BITMODETIMER2   MMIO32(TIMER2 + 0x508)
+// Timer bit width
+#define BITMODE08   0x1
+#define BITMODE16   0x0
+#define BITMODE24   0x2
+#define BITMODE32   0x3
+
+/* Timer prescaler register */
+#define PRESCALERTIMER0 MMIO32(TIMER0 + 0x510)
+#define PRESCALERTIMER1 MMIO32(TIMER1 + 0x510)
+#define PRESCALERTIMER2 MMIO32(TIMER2 + 0x510)
+// Prescaler value. PRESCALER[3:0]
+
+/* Capture/Compare register n */
+#define CC0TIMER0       MMIO32(TIMER0 + 0x540)
+#define CC0TIMER1       MMIO32(TIMER1 + 0x540)
+#define CC0TIMER2       MMIO32(TIMER2 + 0x540)
+#define CC1TIMER0       MMIO32(TIMER0 + 0x544)
+#define CC1TIMER1       MMIO32(TIMER1 + 0x544)
+#define CC1TIMER2       MMIO32(TIMER2 + 0x544)
+#define CC2TIMER0       MMIO32(TIMER0 + 0x548)
+#define CC2TIMER1       MMIO32(TIMER1 + 0x548)
+#define CC2TIMER2       MMIO32(TIMER2 + 0x548)
+#define CC3TIMER0       MMIO32(TIMER0 + 0x54c)
+#define CC3TIMER1       MMIO32(TIMER1 + 0x54c)
+#define CC3TIMER2       MMIO32(TIMER2 + 0x54c)
+// Capture/Compare value. CC[31:0]
 
 #endif
