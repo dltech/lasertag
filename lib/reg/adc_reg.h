53ba0467b600f69ae15a6dc037c3ee5a22a664cb
Константин Скорость <dltech174@gmail.com>
2022-06-03 05:20:36 +0700

    adc regs

--- lib/reg/adc_reg.h
+++ lib/reg/adc_reg.h
@@ -20,19 +20,74 @@
  */
 #include "memorymap.h"
 
-
 /*************Tasks************************/
-/*  */
-#define START   MMIO32(ADC + 0x000)
-
+/* Start a new ADC conversion */
+#define STARTADC    MMIO32(ADC + 0x000)
+/* Stop ADC */
+#define STOPADC     MMIO32(ADC + 0x004)
 
 /************Events************************/
-/*  */
-#define READY   MMIO32(ADC + 0x100)
+/* An ADC conversion is completed */
+#define ENDADC      MMIO32(ADC + 0x100)
 
 /************Registers*********************/
-/*  */
-#define SHORTS        MMIO32(ADC + 0x200)
+/* Enable or disable interrupt */
+#define INTENADC    MMIO32(ADC + 0x300)
+// Enable or disable interrupt on END event
+#define ENDADC  0x1
 
+/* Enable interrupt */
+#define INTENSETADC MMIO32(ADC + 0x304)
+// Write '1' to Enable interrupt on END event.
+
+/* Disable interrupt */
+#define INTENCLRADC MMIO32(ADC + 0x308)
+// Write '1' to Clear interrupt on END event.
+
+/* ADC busy (conversion in progress) */
+#define BUSYADC     MMIO32(ADC + 0x400)
+// 1 = ADC is busy. Conversion in progress.
+#define BUSYA   0x1
+
+/* Enable ADC */
+#define ENABLEADC   MMIO32(ADC + 0x500)
+// ADC enable
+#define ENABLEAD    0x1
+
+/* ADC configuration */
+#define CONFIGADC   MMIO32(ADC + 0x504)
+// ADC resolution
+#define RES8BIT  0x0
+#define RES9BIT  0x1
+#define RES10BIT 0x2
+// ADC input selection
+#define INPSEL_AnalogInputNoPrescaling  0x00
+#define INPSEL_AnalogInput2_3Prescaling 0x04
+#define INPSEL_AnalogInput1_3Prescaling 0x08
+#define INPSEL_Supply2_3Prescaling      0x14
+#define INPSEL_Supply1_3Prescaling      0x18
+// ADC reference selection
+#define REFSEL_VBG                      0x00
+#define REFSEL_External                 0x20
+#define REFSEL_Supply1_2Prescaling      0x40
+#define REFSEL_Supply1_3Prescaling      0x60
+// Select pin to be used as ADC input pin
+#define PSEL_DIS                        0x0000
+#define PSEL_ANA0                       0x0100
+#define PSEL_ANA1                       0x0200
+#define PSEL_ANA2                       0x0400
+#define PSEL_ANA3                       0x0800
+#define PSEL_ANA4                       0x1000
+#define PSEL_ANA5                       0x2000
+#define PSEL_ANA6                       0x4000
+#define PSEL_ANA7                       0x8000
+// External reference pin selection
+#define EXTREFSEL_NONE      0x00000
+#define EXTREFSEL_ANAREF0   0x10000
+#define EXTREFSEL_ANAREF1   0x20000
+
+/* Result of the previous ADC conversion */
+#define RESULTADC   MMIO32(ADC + 0x508)
+// Result of the previous ADC conversion. RESULT[9:0]
 
 #endif
